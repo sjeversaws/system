7efdf49dc425ca071b5be791fd68e44cd6885c93
Steven Evers <sjevers@amazon.com>
2022-11-07 11:16:07 -0700

    update helm

--- demo-app/locals.tf
+++ demo-app/locals.tf
@@ -21,11 +21,8 @@ locals {
   workload_application = {
     name                = "systems"
     path                = "helm"
-    repo_url            = "git@ssh.gitlab.aws.dev:sjevers/system.git"
-    project             = "default"
+    repo_url            = "https://github.com/sjeversaws/system.git"
     add_on_application  = false         # Indicates the root add-on application.
-    ssh_key_secret_name = "gitlab/ssh"  # Needed for private repos
-    insecure            = false         # Set to true to disable the server's certificate verification
   }
 
   tags = {
